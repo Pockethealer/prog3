676d23d0d1e688417944c4fe9c95d30baf44607e
Pockethealer <laszlo.m.grob@gmail.com>
2025-09-26 15:25:35 +0200

    lab 3

---
+++ lab3/lab3.java
@@ -0,0 +1,12 @@
+package lab3;
+
+import java.io.IOException;
+
+public class lab3 {
+
+    public static void main(String[] Args) throws IOException {
+        Shell shell = new Shell();
+        shell.run();
+    }
+
+}
