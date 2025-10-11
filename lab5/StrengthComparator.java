67f4dd04b65932fc3b061d8f7c660be92b4fc943
Pockethealer <laszlo.m.grob@gmail.com>
2025-10-11 13:57:15 +0200

    lab5

---
+++ lab5/StrengthComparator.java
@@ -0,0 +1,9 @@
+package lab5;
+
+import java.util.Comparator;
+
+public class StrengthComparator implements Comparator<Beer> {
+    public int compare(Beer b1, Beer b2) {
+        return Double.compare(b1.getErosseg(), b2.getErosseg());
+    }
+}
